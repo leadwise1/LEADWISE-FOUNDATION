d585c95a660b5b9d3f10a1aa74e2917068f85a45
LeadWise Foundation <sales@causevay.com>
2026-02-06 04:22:16 -0600

    Integrate Google Tag Manager for analytics
    Added Google Analytics tracking script to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script'
 import { Cinzel, Oswald } from 'next/font/google'
 import type { Metadata, Viewport } from 'next'
 
@@ -32,9 +33,27 @@ export default function RootLayout({
   return (
     <html lang="en" dir="ltr" className={`${cinzel.variable} ${oswald.variable}`}>
       <head>
+        {/* Google Tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FY2TX3SEMV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FY2TX3SEMV');
+          `}
+        </Script>
+
         {/* Preconnects for Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
       </head>
       <body className="font-oswald font-sans bg-cream text-navy overflow-x-hidden">
         {children}
