e57886fcb989308cf25b3746943fb44a57cac4b6
LeadWise Foundation <sales@causevay.com>
2026-02-21 17:18:28 -0600

    Refactor Hero component layout for responsiveness

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,38 +33,37 @@ const Hero: React.FC = () => {
             </a>
           </div>
 
-          {/* Column 2 */}
-          <div className="relative aspect-[720/600] lg:aspect-[300/600] rounded-2xl overflow-hidden shadow-xl">
-            <Image
-              src="/images/hero.png"
-              alt="Leadership Development"
-              fill
-              priority
-              className="object-cover"
-            />
-          </div>
+         {/* Column 2 */}
+<div className="relative h-full min-h-[420px] rounded-2xl overflow-hidden shadow-xl">
+  <Image
+    src="/images/hero.png"
+    alt="Leadership Development"
+    fill
+    priority
+    className="object-cover"
+  />
+</div>
 
-          {/* Column 3 */}
-          <div className="space-y-4">
-            <div className="relative aspect-[290/300] lg:aspect-[300/300] rounded-2xl overflow-hidden shadow-xl">
-              <Image
-                src="/images/hero2.png"
-                alt="Collaborative Learning"
-                fill
-                className="object-cover"
-              />
-            </div>
+{/* Column 3 */}
+<div className="flex flex-col gap-4 h-full">
+  <div className="relative flex-1 min-h-[200px] rounded-2xl overflow-hidden shadow-xl">
+    <Image
+      src="/images/hero2.png"
+      alt="Collaborative Learning"
+      fill
+      className="object-cover"
+    />
+  </div>
 
-            <div className="relative aspect-[290/300] lg:aspect-[300/300] rounded-2xl overflow-hidden shadow-xl">
-              <Image
-                src="/images/hero3.png"
-                alt="Mentorship"
-                fill
-                className="object-cover"
-              />
-            </div>
-          </div>
-
+  <div className="relative flex-1 min-h-[200px] rounded-2xl overflow-hidden shadow-xl">
+    <Image
+      src="/images/hero3.png"
+      alt="Mentorship"
+      fill
+      className="object-cover"
+    />
+  </div>
+</div>
         </div>
       </div>
     </section>
